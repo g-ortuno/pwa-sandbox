4950a3965e0e12a44f7e0134d399c927541c9cbd
Giovanni Ortuño Urquidi <gd.ortuno@gmail.com>
2017-06-26 17:55:04 +1000

    add offline

--- sw.ts
+++ sw.ts
@@ -1,3 +1,23 @@
-self.addEventListener('install', event => {
-    console.log('yay');
+self.addEventListener('install', (event: ExtendableEvent) => {
+    event.waitUntil((async () => {
+        let cache = await caches.open('sandbox-v1');
+        await cache.addAll(['index.html']);
+        console.log('cached everything');
+        let keys = await cache.keys();
+        for(let key in keys) {
+            console.log(key);
+        }
+    })());
+});
+
+self.addEventListener('fetch', (event: FetchEvent) => {
+    event.respondWith((async () => {
+        try {
+            let response = await caches.match(event.request);
+            return response || fetch(event.request);
+        } catch (error) {
+            console.log(error);
+            return caches.match('index.html');
+        }
+    })());
 });
