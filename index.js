edb6c5ca9a703f421767d7e27aa67f68f5e71649
Giovanni Ortuno <g-ortuno@users.noreply.github.com>
2017-09-13 21:59:20 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ window.addEventListener('load', async () => {
     let reg = await navigator.serviceWorker.register('sw.js');
     console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n', reg);
 });
+
+function openWindow() {
+    let t = window.open('https://example.com');
+    t.location.href = 'https://pokedex.org';
+}
